98d50c71245e5975e00381c5bec5c5f2fbe713d5
Eric Bolikowski <eric@norappweb.com>
2019-01-20 22:05:00 +0100

    Refurbish map data preprocessing for 'offsetFactor' and 'renderers'

--- src/utils.js
+++ src/utils.js
@@ -1,39 +1,19 @@
+import flow from 'lodash/fp/flow'
+import groupBy from 'lodash/fp/groupBy'
+import filter from 'lodash/fp/filter'
+import transform from 'lodash/fp/transform'
+import map from 'lodash/fp/map'
+import join from 'lodash/fp/join'
+import sum from 'lodash/fp/sum'
+import flatten from 'lodash/fp/flatten'
+import pick from 'lodash/fp/pick'
+
 export const eventHasLatitudeAndLongitude = event =>
-  event.latitude && event.longitude;
+    event.latitude && event.longitude;
 export const eventToLatLng = event =>
-  event.latitude.toString() + event.longitude.toString();
+    event.latitude.toString() + event.longitude.toString();
 export const eventToTypeLatLng = event =>
-  event.displacement_type + eventToLatLng(event);
-export const eventLatLngSortComparer = (event1, event2) => {
-  const latLng1 = eventToTypeLatLng(event1);
-  const latLng2 = eventToTypeLatLng(event2);
-  return latLng1 - latLng2;
-};
-export const mergeEventsWithSameCoordsAndTypeReducer = (acc, curr) => {
-  const last = acc[acc.length - 1];
-  if (!last) return [curr];
-  const lastTypeLatLng = eventToTypeLatLng(last);
-  const currTypeLatLng = eventToTypeLatLng(curr);
-  if (lastTypeLatLng === currTypeLatLng) {
-    last.standard_info_text += curr.standard_info_text;
-    last.standard_popup_text += `<br /><br />${curr.standard_popup_text}`;
-    last.figure += curr.figure;
-  } else {
-    acc.push(curr);
-  }
-  return acc;
-};
-export const markEventsWithSameCoordsForOffsetReducer = (acc, curr) => {
-  const last = acc[acc.length - 1];
-  if (!last) return [curr];
-  const lastLatLng = eventToLatLng(last);
-  const currLatLng = eventToLatLng(curr);
-  if (lastLatLng === currLatLng) {
-    curr.offsetFactor = true;
-  }
-  acc.push(curr);
-  return acc;
-};
+    event.displacement_type + eventToLatLng(event);
 
 export const mapDataToGeoJson = results => ({
   type: "FeatureCollection",
@@ -49,8 +29,53 @@ export const mapDataToGeoJson = results => ({
       },
       geometry: {
         type: "Point",
-        coordinates: [item.longitude, item.latitude]
+        coordinates: [ item.longitude, item.latitude ]
       }
     };
   })
 });
+
+const mergeEvents = (events) => {
+  const singleEvent = {
+    ...events[ 0 ]
+  };
+  singleEvent.standard_info_text = flow(
+      map(e => e.standard_info_text),
+      join(''),
+  )(events);
+  singleEvent.standard_popup_text = flow(
+      map(e => e.standard_popup_text),
+      join('<br /><br />'),
+  )(events);
+  singleEvent.figure = flow(
+      map(e => e.figure),
+      sum,
+  )(events);
+  return singleEvent
+};
+
+export const rawDataToOffsetGrouped = rawData => {
+  let { single: eventsWithUniqueCoords, multiple: eventsWithSharedCoords } = flow(
+      map(v => pick([ 'latitude', 'longitude', 'displacement_type', 'figure', 'standard_info_text', 'standard_popup_text', 'displacement_date', 'displacement_start_date' ])(v)),
+      filter(eventHasLatitudeAndLongitude),
+      groupBy(eventToTypeLatLng),
+      transform((acc, v) => {
+        acc[ v.length > 1 ? 'multiple' : 'single' ].push(...v);
+      }, { single: [], multiple: [] })
+  )(rawData);
+
+  const eventsWithSharedCoordsMergedAndOffsetMarked = flow(
+      groupBy(eventToTypeLatLng),
+      map(mergeEvents),
+      groupBy(eventToLatLng),
+      map(eventsAtSameCoords =>
+          eventsAtSameCoords.map((v, i) => Object.assign(v, { offsetFactor: i }))
+      ),
+      flatten,
+  )(eventsWithSharedCoords);
+  const all = [
+    ...eventsWithUniqueCoords.map(e => Object.assign(e, { offsetFactor: 0 })),
+    ...eventsWithSharedCoordsMergedAndOffsetMarked
+  ];
+  return groupBy('offsetFactor', all)
+};
